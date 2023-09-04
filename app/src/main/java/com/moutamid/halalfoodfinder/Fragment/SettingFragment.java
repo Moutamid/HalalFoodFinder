0a0444196e9970ca822d26e01f43fea250f83418
Fiza <arbkan05@gmail.com>
2023-09-04 05:54:24 -0700

    changes  1-09-2023

--- app/src/main/java/com/moutamid/halalfoodfinder/Fragment/SettingFragment.java
+++ app/src/main/java/com/moutamid/halalfoodfinder/Fragment/SettingFragment.java
@@ -85,6 +85,7 @@ public class SettingFragment extends Fragment {
         });
 
 
+
         share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
